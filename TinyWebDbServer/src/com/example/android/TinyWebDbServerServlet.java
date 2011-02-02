1727f560ee26cce412a506731025fe73d7db5906
markuskhouri <markuskhouri@gmail.com>
2011-02-02 03:59:10 +0000

    added compatibility for Voting block (http://appinventor.googlelabs.com/learn/reference/components/notready.html#Voting)

--- TinyWebDbServer/src/com/example/android/TinyWebDbServerServlet.java
+++ TinyWebDbServer/src/com/example/android/TinyWebDbServerServlet.java
@@ -2,6 +2,7 @@ package com.example.android;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +23,18 @@ public class TinyWebDbServerServlet extends HttpServlet
     public static final String TINY_WEB_DB_RETURN_KEYWORD_VALUE = "VALUE";
     public static final String TINY_WEB_DB_URI_GET_VALUE = "/getvalue";
     public static final String TINY_WEB_DB_URI_STORE_A_VALUE = "/storeavalue";
+
+    // For notes on Voting, see 
+    // http://appinventor.googlelabs.com/forum/index.html#!msg/appinventor/YCZdpvupTrE/Dg6MlrE8DKoJ
+    public static final String VOTING_URI_REQUEST_BALLOT = "/requestballot";
+    public static final String VOTING_URI_SEND_BALLOT = "/sendballot";
+    public static final String VOTING_PARAMETER_USER_CHOICE = "userchoice";
+    public static final String VOTING_PARAMETER_USER_ID = "userid";
+    public static final String VOTING_IS_POLLING = "isPolling";
+    public static final String VOTING_ID_REQUESTED = "idRequested";
+    public static final String VOTING_QUESTION = "question";
+    public static final String VOTING_OPTIONS = "options";
+    
     public static final String VALUE_DELIMETER = "|";
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
@@ -30,6 +43,8 @@ public class TinyWebDbServerServlet extends HttpServlet
 	    if (logger.isLoggable(Level.FINE))
 		{
 			logger.fine("action-GET = [" + action + "]");
+
+			logAllParameters(request);
 		} // end of IF
 		
 		response.setContentType("text/plain");
@@ -43,6 +58,8 @@ public class TinyWebDbServerServlet extends HttpServlet
 	    if (logger.isLoggable(Level.FINE))
 		{
 			logger.fine("action-POST = [" + action + "]");
+			
+			logAllParameters(request);
 		} // end of IF
 
 	    if (action.equals(TINY_WEB_DB_URI_GET_VALUE))
@@ -52,6 +69,18 @@ public class TinyWebDbServerServlet extends HttpServlet
 		else if (action.equals(TINY_WEB_DB_URI_STORE_A_VALUE))
 		{
 			processStoreAValueRequest(request, response);
+		} // end of ELSE
+		else if (action.equals(TINY_WEB_DB_URI_STORE_A_VALUE))
+		{
+			processStoreAValueRequest(request, response);
+		} // end of ELSE
+		else if (action.equals(VOTING_URI_REQUEST_BALLOT))
+		{
+			processRequestBallotRequest(request, response);
+		} // end of ELSE
+		else if (action.equals(VOTING_URI_SEND_BALLOT))
+		{
+			processSendBallotRequest(request, response);
 		} // end of ELSE
 		else
 		{
@@ -249,4 +278,126 @@ public class TinyWebDbServerServlet extends HttpServlet
 		// String strippedValue = value.substring(1, value.length()-1);
 
 	} // end of storeValueAssociatedWithTag()
+    
+    public void processRequestBallotRequest(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+    	// based on information taken from
+    	// http://androvote.appspot.com/requestballot
+    	//The server will send this to the component:
+    	//{"isPolling": true, "idRequested": true, "question": "dfhdfh", "options": ["dfhdfh", "dfh"]}
+
+    	boolean isPollOpen = true;
+    	boolean isUserIdRequested = true;
+    	String pollQuestion = "What is the weather like?";
+    	String pollOption1 = "raining";
+    	String pollOption2 = "clear";
+    	
+    	//String text = "{\""+VOTING_IS_POLLING+"\": "+isPollOpen+", \""+VOTING_ID_REQUESTED+"\": "+isUserIdRequested+", \""+VOTING_QUESTION+"\": \""+pollQuestion+"\", \""+VOTING_OPTIONS+"\": [\""+pollOption1+"\", \""+pollOption2+"\"]}"; 
+
+    	//String temp = null;
+    	//temp= "{\"isPolling\": true, \"idRequested\": true, \"question\": \"What is the weather like?\", \"options\": [\"raining\", \"clear\"]}"; 
+    	//logger.fine(temp);
+    	//temp = "{\""+VOTING_IS_POLLING+"\": "+isPollOpen+", \""+VOTING_ID_REQUESTED+"\": "+isUserIdRequested+", \""+VOTING_QUESTION+"\": \""+pollQuestion+"\", \""+VOTING_OPTIONS+"\": [\""+pollOption1+"\", \""+pollOption2+"\"]}"; 
+    	//logger.fine(temp);
+    	
+    	// TODO: fix this... is this really a JSON formatted object?
+    	// TODO: Create a class named 'Poll', with a boolean 'isPollOpen', 
+    	//		a boolean 'isUserIdRequested', a String parameter 'question', 
+    	//		and a List<String> parameter 'options'.
+    	// Then, loop through 'options' parameter below.
+    	
+    	StringBuffer sb = new StringBuffer();
+    	sb.append("{\"").append(VOTING_IS_POLLING).append("\": ").append(isPollOpen).append(", \"").append(VOTING_ID_REQUESTED).append("\": ").append(isUserIdRequested).append(", \"").append(VOTING_QUESTION).append("\": \"").append(pollQuestion).append("\", \"").append(VOTING_OPTIONS).append("\": [\"").append(pollOption1).append("\", \"").append(pollOption2).append("\"]}");
+    	String text = null; 
+    	text = sb.toString();
+    	logger.fine(text);
+    	
+        // send the result back
+        response.setContentType("application/jsonrequest");
+        response.getWriter().print(text);
+
+    } // end of processRequestBallotRequest()
+
+    public void processSendBallotRequest(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+
+    	// get the "userChoice" and "userId" parameters
+        String userChoice = request.getParameter(VOTING_PARAMETER_USER_CHOICE);
+        String userId = request.getParameter(VOTING_PARAMETER_USER_ID);
+
+        if (logger.isLoggable(Level.FINE))
+		{
+            logger.fine("userChoice = [" + userChoice + "]");
+            logger.fine("userId = [" + userId + "]");
+		} // end of IF
+        
+        // This is the "guts" of the call.
+		//storeBallotResponse(userChoice, userId);
+
+        // Construct the required JavaScript return object
+        String jsonReturnString = "";
+        String[] dataToReturn = new String[3];
+        Gson gson = new Gson();
+        
+        // FIXME: what should be returned? 
+        // TODO: Return something so that Apps Inventor block 'Voting1.GotBallotConfirmation' is invoked.
+        //		Strangely, anything seems to invoke this block.
+
+        // populate the fields to be returned
+        dataToReturn[0] = "XXX";
+        dataToReturn[1] = userChoice;  // this is returned unchanged; correct?
+        dataToReturn[2] = userId;  // this is returned unchanged; correct?
+
+        // create the JSON return string
+        jsonReturnString = gson.toJson(dataToReturn);
+        if (logger.isLoggable(Level.FINE))
+		{
+			logger.fine("jsonReturnString = [" + jsonReturnString + "]");
+		} // end of IF
+
+        // send the result back
+        response.setContentType("application/jsonrequest");
+        response.getWriter().print(jsonReturnString);
+
+    } // end of processSendBallotRequest()
+    
+	static public void logAllParameters(HttpServletRequest request)
+	{
+		// The following shows all the request parameters
+		Enumeration paramNames = request.getParameterNames();
+		String currentLine = "";
+		while (paramNames.hasMoreElements())
+		{
+			String paramName = (String) paramNames.nextElement();
+			currentLine = ("paramName = [" + paramName + "]");
+			String[] paramValues = request.getParameterValues(paramName);
+			if (paramValues.length == 1)
+			{
+				String paramValue = paramValues[0];
+				if (paramValue.length() == 0)
+				{
+					currentLine += ("No Value");
+				} // end of IF
+				else
+				{
+					currentLine += ("paramValue = [" + paramValue + "]");
+				} // end of ELSE
+			} // end of IF
+			else
+			{
+				StringBuffer sb = new StringBuffer();
+				for (int i = 0; i < paramValues.length; i++)
+				{
+					// currentLine += ("paramValue[" + i + "] = [" + paramValues[i] + "]");
+					sb.append("paramValue[").append(i).append("] = [").append(paramValues[i]).append("]");
+				} // end of FOR
+				currentLine += sb.toString();
+			} // end of ELSE
+			if (logger.isLoggable(Level.FINE))
+			{
+				logger.fine("currentLine = [" + currentLine + "]");
+			} // end of IF
+		} // end of WHILE
+	}
+    
 }
